df4314d7a3b6a901647d508127ed4772c2fb244a
Liang Mingjing <liangmingjing849@163.com>
2025-08-16 17:47:05 +0800

    import hydra to manage configuartion files
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- pendulum/evaluate.py
+++ pendulum/evaluate.py
@@ -1,29 +1,40 @@
+import hydra
 import gymnasium as gym
 
 from stable_baselines3 import SAC
 
 
-def main():
-    env = gym.make("Pendulum-v1", render_mode="human", g=9.81)
+@hydra.main(version_base=None, config_path="config", config_name="evaluate")
+def main(config):
+    env = gym.make(
+        config.env.name,
+        render_mode=config.env.render_mode,
+        g=config.env.gravity
+    )
 
     # Load model
-    model = SAC.load("saved/model/sac_pendulum")
+    model = SAC.load(config.model_path)
 
     # Evaluate
     total_reward = 0.0
 
     observation, info = env.reset()
     while True:
-        action = model.predict(observation, deterministic=True)[0]
-        observation, reward, terminated, truncated, info = env.step(action)
+        
+        try:
+            action = model.predict(observation, deterministic=True)[0]
+            observation, reward, terminated, truncated, info = env.step(action)
 
-        total_reward += reward
-        if terminated or truncated:
-            observation, info = env.reset()
+            total_reward += reward
+            if terminated or truncated:
+                observation, info = env.reset()
 
-            print(f"Total reward: {total_reward}")
+                print(f"Total reward: {total_reward}")
 
-            total_reward = 0.0
+                total_reward = 0.0
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
 
 
 if __name__ == "__main__":
--- pendulum/train.py
+++ pendulum/train.py
@@ -1,15 +1,27 @@
+import hydra
 import gymnasium as gym
 
 from stable_baselines3 import SAC
 
 
-def train():
-    env = gym.make("Pendulum-v1", render_mode="human", g=9.81)
+@hydra.main(version_base=None, config_path="config", config_name="train")
+def main(config):
+    env = gym.make(
+        config.env.name,
+        render_mode=config.env.render_mode,
+        g=config.env.gravity
+    )
 
-    model = SAC("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=10000, log_interval=4)
-    model.save("saved/model/sac_pendulum")
+    model = SAC(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        tensorboard_log=config.tensorboard_log,
+    )
+    model.learn(
+        total_timesteps=config.total_timesteps, log_interval=config.log_interval)
+    model.save(config.model_path)
 
 
 if __name__ == "__main__":
-    train()
+    main()
