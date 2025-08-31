7324bc0b9614554e9217f0d6dbee8a198db9ee7b
Liang Mingjing <liangmingjing849@163.com>
2025-08-31 22:59:34 +0800

    added train_one_episode
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- tabular_q/trainer.py
+++ tabular_q/trainer.py
@@ -5,26 +5,13 @@ class Trainer:
         self.config = config
         self.num_train_episodes = config.get("num_train_episodes", 1000)
         self.max_steps_per_episode = config.get("max_steps_per_episode", 100)
+        self.evaluate_per_episodes = config.get("evaluate_per_episodes", 100)
 
     def train(self):
         for episode in range(self.num_train_episodes):
-            self._train_one_episode()
+            self._train_one_episode(episode)
         
         self.env.close()
-    
-    def _train_one_episode(self):
-        observation, info = self.env.reset()
-        done = False
-        while not done:
-            action = self.agent.predict(observation)
-            observation, reward, terminated, truncated, info = self.env.step(action)
-            print(observation, reward, terminated, truncated, info)
-            self.env.render()
-
-            done = terminated or truncated
-            self.agent.update(observation, reward, done)
-            if done:
-                observation, info = self.env.reset()
     
     def evaluate(self, num_eval_step):
         observation, info = self.env.reset()
@@ -38,3 +25,26 @@ class Trainer:
                 observation, info = self.env.reset()
 
         self.env.close()
+    
+    def _train_one_episode(self, episode):
+        
+        # init
+        observation, info = self.env.reset()
+        done = False
+        step = 0
+
+        while not done:
+            action = self.agent.predict(observation)
+            observation, reward, terminated, truncated, info = self.env.step(action)
+            print(observation, reward, terminated, truncated, info)
+            self.env.render()
+
+            step += 1
+
+            done = terminated or truncated or self._is_reached_max_steps(step)
+            self.agent.update(observation, reward, done)
+            if done:
+                observation, info = self.env.reset()
+    
+    def _is_reached_max_steps(self, step):
+        return step >= self.max_steps_per_episode
