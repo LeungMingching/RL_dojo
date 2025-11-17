4c6d6209a3499e0bb17a7f5788f262c615a5d765
Liang Mingjing <liangmingjing849@163.com>
2025-11-17 17:45:43 +0800

    improved evaluation
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- tabular_q/trainer.py
+++ tabular_q/trainer.py
@@ -1,3 +1,5 @@
+from gymnasium.wrappers import RecordVideo, RecordEpisodeStatistics
+
 import os
 
 
@@ -11,10 +13,18 @@ class Trainer:
         self.max_steps_per_episode = config.get("max_steps_per_episode", 100)
         
         self.evaluate_per_episodes = config.get("evaluate_per_episodes", 100)
-        self.num_evaluate_step = config.get("num_evaluate_step", 10)
+        self.num_evaluate_episodes = config.get("num_evaluate_episodes", 10)
 
         self.save_path = config.get("save_path", "saved")
         self.save_policy_per_episodes = config.get("save_policy_per_episodes", 10)
+
+        self.record_env = RecordVideo(
+            self.env,
+            video_folder=os.path.join(self.save_path, "videos"),
+            name_prefix="evaluation",
+            episode_trigger=lambda x: True
+        )
+        self.record_env = RecordEpisodeStatistics(self.record_env)
 
     def train(self):
         for episode in range(self.num_train_episodes):
@@ -28,23 +38,16 @@ class Trainer:
             if self._is_reached_save_episode(episode):
                 model_dir = f"{self.save_path}/models"
                 os.makedirs(model_dir,exist_ok=True)
-                file_path = os.path.join(model_dir, f"episode_{episode+1}.npy")
+                file_path = os.path.join(model_dir, f"train_episode_{episode+1}.npy")
                 self.agent.save_policy(file_path)
         
         self.env.close()
-    
+
     def evaluate(self):
-        observation, info = self.env.reset()
-        for step in range(self.num_evaluate_step):
-            action = self.agent.predict(observation, explore=False)
-            observation, reward, terminated, truncated, info = self.env.step(action)
-            self.env.render()
+        for episode in range(self.num_evaluate_episodes):
+            self._evaluate_one_episode(episode)
 
-            done = terminated or truncated
-            if done:
-                observation, info = self.env.reset()
-
-        self.env.close()
+        self.record_env.close()
     
     def _train_one_episode(self, episode):
         
@@ -56,8 +59,6 @@ class Trainer:
         while not done:
             action = self.agent.predict(observation)
             observation, reward, terminated, truncated, info = self.env.step(action)
-            print(observation, reward, terminated, truncated, info)
-            self.env.render()
 
             step += 1
 
@@ -65,6 +66,25 @@ class Trainer:
             self.agent.update(observation, reward, done)
             if done:
                 observation, info = self.env.reset()
+
+    def _evaluate_one_episode(self, episode):
+        print(f"Evaluation Episode {episode + 1}/{self.num_evaluate_episodes}")
+        print(type(self.record_env), getattr(self.record_env.env, "render_mode", None))
+        
+        # init
+        observation, info = self.record_env.reset()
+        done = False
+        step = 0
+
+        while not done:
+            action = self.agent.predict(observation, explore=False)
+            observation, reward, terminated, truncated, info = self.record_env.step(action)
+
+            step += 1
+
+            done = terminated or truncated or self._is_reached_max_steps(step)
+            if done:
+                observation, info = self.record_env.reset()
     
     def _is_reached_max_steps(self, step):
         return step >= self.max_steps_per_episode
